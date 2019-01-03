f61acce81d388067dc8de8e3b2d5a6727e692dc4
AntonioCerasuolo <AntonioCerasuolo@users.noreply.github.com>
2019-01-03 16:53:25 +0100

    a

--- SmartLab/src/presentation/servlet/ServletPostazioneManagement.java
+++ SmartLab/src/presentation/servlet/ServletPostazioneManagement.java
@@ -19,10 +19,6 @@ public class ServletPostazioneManagement extends HttpServlet {
      */
     public ServletPostazioneManagement() {
         super();
-        asdasdasd
-        asd
-        das
-        ads
         // TODO Auto-generated constructor stub
     }
 
