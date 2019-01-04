495f492fa84c1f0e594735ed1420d3b65caa63fa
giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>
2019-01-04 10:52:18 +0100

    Signed-off-by: giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>

--- src/businessLogic/laboratorio/ListaLab.java
+++ src/businessLogic/laboratorio/ListaLab.java
@@ -2,6 +2,17 @@ package businessLogic.laboratorio;
 import dataAccess.storage.SqlSpecification;
 
 public class ListaLab implements SqlSpecification{
+	
+	private static ListaLab instance;
+
+    public static ListaLab getInstance() {
+
+        if (instance == null) {
+            instance = new ListaLab();
+        }
+        return instance;
+
+    }
 
     public static final String TABLE_NAME = "laboratorio";//nome della tabella su cui saranno effettuate le operazioni
 
---
+++ test/laboratorioTest/ListaLabTest.java
@@ -0,0 +1,27 @@
+package laboratorioTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import businessLogic.laboratorio.ListaLab;
+
+public class ListaLabTest {
+	
+	@Test
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        ListaLab result = ListaLab.getInstance();
+        assertNotNull(result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+	}
+	
+	@Test
+	public void testToSqlQuery() {
+		ListaLab lista=new ListaLab();
+		ListaLab result=ListaLab.getInstance();
+		assertEquals(lista,result);
+	}
+
+}
