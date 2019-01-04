604f52e0ff6e3e908a2e6b027c87d69382662cc7
giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>
2019-01-04 11:02:25 +0100

    Signed-off-by: giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>

--- test/laboratorioTest/LaboratorioSqlTest.java
+++ test/laboratorioTest/LaboratorioSqlTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class LaboratorioSqlTest {
+import businessLogic.laboratorio.LaboratorioSql;
+import businessLogic.laboratorio.ListaLab;
 
+public class LaboratorioSqlTest {
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        LaboratorioSql result = LaboratorioSql.getInstance("");
+        assertNotNull(result);
+        // TODO review the generated test code and remove the default call to fail.
+        
+	}
+	
+	@Test
+	public void testToSqlQuery() {
+		
 	}
 
 }
