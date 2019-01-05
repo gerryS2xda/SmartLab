bf2688dbcb21d732347c8af8bfc37884ceed2e0b
giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>
2019-01-05 12:25:11 +0100

    Signed-off-by: giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>

---
+++ test/assegnamentoTest/AssegnamentoManagerTest.java
@@ -0,0 +1,14 @@
+package assegnamentoTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssegnamentoManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
