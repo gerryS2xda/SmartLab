e246f239a19ffd4af7dea59757189f84b1c11ca4
gerryS2xda <gerardo96bn@hotmail.it>
2019-01-10 21:52:51 +0100

    Remove fascia_oraria attribute and add ora_inizio, ora_fine

--- sql/prenotazione_sql.sql
+++ sql/prenotazione_sql.sql
@@ -8,14 +8,15 @@ DROP TABLE IF EXISTS `prenotazione`;
 CREATE TABLE Prenotazione(
 	IDprenotazione INTEGER(10) PRIMARY KEY auto_increment,
     data DATE  NOT NULL,
-    fascia_oraria VARCHAR(30) NOT NULL,
+    ora_inizio Time NOT NULL,
+    ora_fine Time NOT NULL,
     stato boolean NOT NULL,
     studente VARCHAR(50),
     postazione smallint,
     laboratorio tinyint
 );
 
-INSERT INTO prenotazione(data, fascia_oraria, stato, studente, postazione, laboratorio)
-	VALUES ('2018-12-28', '9 - 11', true, 'g.laucella@studenti.unisa.it', 120, 2); 
-INSERT INTO prenotazione(data, fascia_oraria, stato, studente, postazione, laboratorio)
-	VALUES ('2018-12-28', '11 - 13', true, 'g.laucella@studenti.unisa.it', 120, 2);
+INSERT INTO prenotazione(data, ora_inizio, ora_fine, stato, studente, postazione, laboratorio)
+	VALUES ('2018-12-28', '9:00', '11:00', true, 'g.laucella@studenti.unisa.it', 120, 2); 
+INSERT INTO prenotazione(data, ora_inizio, ora_fine, stato, studente, postazione, laboratorio)
+	VALUES ('2018-12-28', '11:00', '13:00', true, 'g.laucella@studenti.unisa.it', 120, 2)
