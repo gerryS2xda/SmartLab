c76cad238900fea60fa69ea45c5bd61dc77777f0
FulvioS <fulvio309@gmail.com>
2019-01-22 18:56:26 +0100

    Aggiornamento dei metodi

--- src/businessLogic/comunicazione/CommunicationManager.java
+++ src/businessLogic/comunicazione/CommunicationManager.java
@@ -9,23 +9,37 @@ import dataAccess.storage.bean.Segnalazione;
 
 public class CommunicationManager {
 	
-	public void addSegnalazione(Segnalazione s){
-		SegnalazioneRepository sr = new SegnalazioneRepository();
-		try {
-			sr.add(s);
-		} catch (SQLException e) {
-			System.out.println("Errore durante l'inserimento della segnalazione");
-			e.printStackTrace();
+	public boolean addSegnalazione(Segnalazione s){
+		if(s != null){
+			SegnalazioneRepository sr = new SegnalazioneRepository();
+			try {
+				sr.add(s);
+				return true;
+			} catch (SQLException e) {
+				System.out.println("Errore durante l'inserimento della segnalazione");
+				e.printStackTrace();
+				return false;
+			}
+		}else{
+			System.out.println("Segnalazione vuota");
+			return false;
 		}
 	}
 	
-	public void deleteSegnalazione(Segnalazione s){
-		SegnalazioneRepository sr = new SegnalazioneRepository();
-		try {
-			sr.delete(s);
-		} catch (SQLException e) {
-			System.out.println("Errore durante l'eliminazione della segnalazione");
-			e.printStackTrace();
+	public boolean deleteSegnalazione(Segnalazione s){
+		if(s != null){
+			SegnalazioneRepository sr = new SegnalazioneRepository();
+			try {
+				sr.delete(s);
+				return true;
+			} catch (SQLException e) {
+				System.out.println("Errore durante l'eliminazione della segnalazione");
+				e.printStackTrace();
+				return false;
+			}
+		}else{
+			System.out.println("Segnalazione vuota");
+			return false;
 		}
 	}
 	
@@ -41,23 +55,37 @@ public class CommunicationManager {
 		return lista;
 	}
 	
-	public void addAvviso(Avviso av){
-		AvvisoRepository ar = new AvvisoRepository();
-		try {
-			ar.add(av);
-		} catch (SQLException e) {
-			System.out.println("Errore durante l'inserimento dell'avviso");
-			e.printStackTrace();
+	public boolean addAvviso(Avviso av){
+		if(av != null){
+			AvvisoRepository ar = new AvvisoRepository();
+			try {
+				ar.add(av);
+				return true;
+			} catch (SQLException e) {
+				System.out.println("Errore durante l'inserimento dell'avviso");
+				e.printStackTrace();
+				return false;
+			}
+		}else{
+			System.out.println("Avviso vuoto");
+			return false;
 		}
 	}
 	
-	public void deleteAvviso(Avviso av){
-		AvvisoRepository ar = new AvvisoRepository();
-		try{
-			ar.delete(av);
-		}catch(SQLException e){
-			System.out.println("Errore durante la cancellazione dell'avviso");
-			e.printStackTrace();
+	public boolean deleteAvviso(Avviso av){
+		if(av != null){
+			AvvisoRepository ar = new AvvisoRepository();
+			try{
+				ar.delete(av);
+				return true;
+			}catch(SQLException e){
+				System.out.println("Errore durante la cancellazione dell'avviso");
+				e.printStackTrace();
+				return false;
+			}
+		}else{
+			System.out.println("Avviso vuoto");
+			return false;
 		}
 	}
 	
