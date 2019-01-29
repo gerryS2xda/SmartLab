a89cd3493a338e3065dfe0b93c72bdb1650599f5
giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>
2019-01-29 17:46:14 +0100

    Signed-off-by: giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>

---
+++ src/presentation/servlet/ServletLaboratorio.java
@@ -0,0 +1,56 @@
+package presentation.servlet;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import businessLogic.laboratorio.LaboratorioManager;
+import dataAccess.storage.bean.Laboratorio;
+
+/**
+ * Servlet implementation class ServletLaboratorio
+ */
+@WebServlet("/ServletLaboratorio")
+public class ServletLaboratorio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	LaboratorioManager manager=new LaboratorioManager();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action=request.getParameter("action");
+		
+		if(action == null){
+			response.setStatus(404);
+			response.sendRedirect("./index.jsp");
+		}else if(action.equals("aggiungi_lab")){
+			Laboratorio lab=new Laboratorio();
+			lab.setNome(request.getParameter("nome"));
+			lab.setPosti(Integer.parseInt(request.getParameter("posti")));
+			lab.setStato(Boolean.parseBoolean(request.getParameter("stato")));
+			
+			/*SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+			long ms = sdf.parse(request.getParameter("apertura")).getTime();*/
+			lab.setApertura(java.sql.Time.valueOf(request.getParameter("apertura")));
+			lab.setChiusura(java.sql.Time.valueOf(request.getParameter("chiusura")));
+			
+			response.setContentType("application/json");
+			response.setCharacterEncoding("utf-8");
+			
+			if(manager.createLaboratory(lab)){
+				response.getWriter().write("{\"esito\":\"laboratorio creato\"}");
+			}else{
+				response.getWriter().write("{\"esito\":\"non è possibile creare il laboratorio\"}");
+			}
+			
+		}
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+}
