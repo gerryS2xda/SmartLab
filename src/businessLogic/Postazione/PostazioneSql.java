8055d598699833ee86ca0c49903fb1675b2213c4
AntonioCerasuolo <AntonioCerasuolo@users.noreply.github.com>
2019-01-31 17:53:46 +0100

    servlet postazione

--- src/businessLogic/Postazione/PostazioneSql.java
+++ src/businessLogic/Postazione/PostazioneSql.java
@@ -9,25 +9,29 @@ public class PostazioneSql implements SqlSpecification {
 	
 	private static final String TABLE_NAME ="Postazione";
 	private int numero;
-	private Laboratorio lab;
-	private boolean stato;
+	private String lab;
 	
-	public PostazioneSql(int num,Laboratorio lab, boolean stato) 
+	public PostazioneSql() 
+	{
+		this.numero=0;
+		this.lab=null;
+	}
+	
+	public PostazioneSql(int num,String lab) 
 	{
 		this.numero=num;
 		this.lab=lab;
-		this.stato=stato;
 	}
 	
 	
 	private static PostazioneSql instance;
 
-    public static PostazioneSql getInstance(int num, Laboratorio lab, boolean stato) 
+    public static PostazioneSql getInstance(int num, String lab) 
     {
 
         if (instance == null) 
         {
-            instance = new PostazioneSql(num,lab,stato);
+            instance = new PostazioneSql(num,lab);
         }
         return instance;
 
@@ -36,7 +40,7 @@ public class PostazioneSql implements SqlSpecification {
 
 	public String toSqlQuery() 
 	{
-		return String.format("SELECT * FROM %1$s WHERE numero = %2$d && IDlaboratorio = %3$s ",TABLE_NAME, this.numero, this.lab, this.stato);
+		return String.format("SELECT * FROM %1$s WHERE numero = %2$d && IDlaboratorio = %3$s ",TABLE_NAME, this.numero, this.lab);
 		
 	}
 
