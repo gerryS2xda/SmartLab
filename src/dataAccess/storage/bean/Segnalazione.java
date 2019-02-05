b3d4de62c85cf92857068cc9c24d85b4ac84ee63
FulvioS <fulvio309@gmail.com>
2019-02-05 22:07:00 +0100

    Eliminazione di alcuni warning

--- src/dataAccess/storage/bean/Segnalazione.java
+++ src/dataAccess/storage/bean/Segnalazione.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.sql.Date;
 
 public class Segnalazione implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
 	private int id;
 	private String oggetto, descrizione, lab;
