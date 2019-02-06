048b7adb517576c01b8e0d19e2198ca323bd17be
FulvioS <fulvio309@gmail.com>
2019-02-06 20:38:40 +0100

    Test case sottosistema comunicazione

--- test/comunicazioneTest/CommunicationManagerTest.java
+++ test/comunicazioneTest/CommunicationManagerTest.java
@@ -1,96 +1,92 @@
 package comunicazioneTest;
 
+import businessLogic.comunicazione.AvvisoRepository;
 import businessLogic.comunicazione.CommunicationManager;
+import businessLogic.comunicazione.SegnalazioneRepository;
 import dataAccess.storage.bean.Avviso;
 import dataAccess.storage.bean.Segnalazione;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.sql.Date;
 import java.util.List;
 
 public class CommunicationManagerTest {
 	
+	private CommunicationManager cm;
+	private AvvisoRepository ar;
+	private SegnalazioneRepository sr;
+	private Avviso oracle;
+	private Segnalazione orcl;
+	
+	@Before
+	public void setUp() throws SQLException{
+		java.util.Date d = new java.util.Date();
+		Date data = new Date(d.getTime());
+		cm = new CommunicationManager();
+		ar = new AvvisoReposistory();
+		sr = new SegnalazioneRepository();
+		oracle = new Avviso();
+		orcl = new Segnalazione();
+		oracle.setId(17);
+		oracle.setMessaggio("Messaggio avviso");
+		oracle.setTitolo("Titolo avviso");
+		oracle.setData(data);
+		oracle.setAddetto("test");
+		orcl.setId(23);
+		orcl.setData(data);
+		orcl.setDescrizione("Descrizione segnalazione");
+		orcl.setOggetto("Oggetto segnalazione");
+		orcl.setLaboratorio("SesaLab");
+		orcl.setPostazione(12);
+		orcl.setStudente("test");
+		cm.addAvviso(oracle);
+		cm.addSegnalazione(orcl);
+	}
+	
+	@After
+	public void tearDown() throws SQLException{
+		ar.delete(oracle);
+		sr.delete(orcl);
+	}
+	
 	@Test
 	public void testaddSegnalazione(){
-		Segnalazione s = new Segnalazione();
-		s.setId(23);
-		s.setOggetto("Oggetto");
-		s.setDescrizione("Descrizione");
-		s.setData(new Data());
-		s.setStudente(12);
-		s.setLaboratorio("SesaLab");
-		s.setPostazione(25);
-		CommunicationManager cm = new CommunicationManager();
-		assertTrue(cm.addSegnalazione(s));
+		Segnalazione res = sr.findItemByQuery(new SegnalazioneSql(orcl.getId()));
+		assertEquals(res, orcl);
 	}
 	
 	@Test
 	public void testDeleteSegnalazione(){
-		Segnalazione s = new Segnalazione();
-		s.setId(23);
-		s.setOggetto("Oggetto");
-		s.setDescrizione("Descrizione");
-		s.setData(new Data());
-		s.setStudente(12);
-		s.setLaboratorio("SesaLab");
-		s.setPostazione(25);
-		CommunicationManager cm = new CommunicationManager();
-		assertTrue(cm.deleteSegnalazione(s));
+		cm.deleteSegnalazione(orcl);
+		Segnalazione res = sr.findItemByQuery(new SegnalazioneSql(orcl.getId()));
+		assertequals(res, null);
 	}
 	
 	@Test
 	public void testViewSegnalazione(){
-		Segnalazione s = new Segnalazione();
-		s.setId(23);
-		s.setOggetto("Oggetto");
-		s.setDescrizione("Descrizione");
-		s.setData(new Data());
-		s.setStudente(12);
-		s.setLaboratorio("SesaLab");
-		s.setPostazione(25);
 		List<Segnalazione> lista = new ArrayList<Segnalazione>();
-		CommunicationManager cm = new CommunicationManager();
-		cm.addSegnalazione(s);
 		lista = cm.viewSegnalazione();
-		assertTrue(!lista.isEmpty());
+		assertEquals(lista.get(lista.size() - 1), orcl);
 	}
 	
 	@Test
 	public void testAddAvviso(){
-		Avviso a = new Avviso();
-		a.setId(10);
-		a.setTitolo("Titolo");
-		a.setMessaggio("Messaggio");
-		a.setData(new Data());
-		a.setAddetto(34);
-		CommunicationManager cm = new CommunicationManager();
-		assertTrue(cm.addAvviso(a));
+		Avviso rs = ar.findItemByQuery(new AvvisoSql(oracle.getId()));
+		assertEquals(rs, oracle);
 	}
 	
 	@Test
 	public void testDeleteAvviso(){
-		Avviso a = new Avviso();
-		a.setId(10);
-		a.setTitolo("Titolo");
-		a.setMessaggio("Messaggio");
-		a.setData(new Data());
-		a.setAddetto(34);
-		CommunicationManager cm = new CommunicationManager();
-		assertTrue(cm.deleteAvviso(a));
+		cm.deleteAvviso(oracle);
+		Avviso rs = ar.findItemByQuery(new AvvisoSql(oracle.getId()));
+		assertEquals(rs, null);
 	}
 	
 	@Test
 	public void testViewAvviso(){
-		Avviso a = new Avviso();
-		a.setId(10);
-		a.setTitolo("Titolo");
-		a.setMessaggio("Messaggio");
-		a.setData(new Data());
-		a.setAddetto(34);
-		CommunicationManager cm = new CommunicationManager();
-		cm.addAvviso(a);
 		List<Avviso> lista = new ArrayList<Avviso>();
 		lista = cm.viewAvviso();
-		assertTrue(!lista.isEmpty());
+		assertEquals(lista.get(lista.size() - 1), oracle);
 	}
 }
