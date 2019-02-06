de690a4900c3e55ab38ccd2789bf6be8f89dc8b1
giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>
2019-02-06 18:41:55 +0100

    Assegnamento manager test quasi finito
    Signed-off-by: giuseppepaolisi <45846780+giuseppepaolisi@users.noreply.github.com>

--- test/assegnamentoTest/AssegnamentoManagerTest.java
+++ test/assegnamentoTest/AssegnamentoManagerTest.java
@@ -3,16 +3,43 @@ package assegnamentoTest;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import businessLogic.Postazione.PostazioneRepository;
 import businessLogic.assegnamento.AssegnamentoManager;
+import businessLogic.assegnamento.AssegnamentoRepository;
+import businessLogic.assegnamento.AssegnamentoSql;
+import businessLogic.laboratorio.IdLab;
+import businessLogic.laboratorio.LaboratorioManager;
 import businessLogic.laboratorio.LaboratorioRepository;
 import dataAccess.storage.bean.Assegnamento;
+import dataAccess.storage.bean.Laboratorio;
+import dataAccess.storage.bean.Postazione;
 
 public class AssegnamentoManagerTest {
+	private AssegnamentoManager manager;
+	private AssegnamentoRepository repository;
+	private Assegnamento oracle;
+	
+	@Before
+	public void setUp() throws Exception {
+		manager=AssegnamentoManager.getInstance();
+		repository=AssegnamentoRepository.getInstance();
+		
+		oracle=new Assegnamento("esempio1@unisa.it","2");
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		repository.delete(oracle);
+	}
 
 	@Test
     public void testGetInstance() {
@@ -25,38 +52,25 @@ public class AssegnamentoManagerTest {
 	@Test
 	public void testSetRespToLab() throws SQLException{
 		System.out.println("setRespToLab");
-		Assegnamento ass= new Assegnamento();
-		ass.setLaboratorio("Lab1");
-		ass.setResponsabile("resp1");
-		//-----------------
-		AssegnamentoManager instance= AssegnamentoManager.getInstance();
-		assertTrue(instance.setRespToLab(ass));
-		instance.removeResponsabile(ass);
+		 manager.setRespToLab(oracle);
+		 Assegnamento result=repository.findItemByQuery(new AssegnamentoSql(oracle.getLaboratorio(),oracle.getResponsabile()));
+		 
+		 assertEquals(result.getLaboratorio(),oracle.getLaboratorio());
 	}
 	
 	@Test
 	public void testRemoveResponsabile() throws SQLException{
 		System.out.println("removeResponsabile");
-		Assegnamento ass= new Assegnamento();
-		ass.setLaboratorio("Lab1");
-		ass.setResponsabile("resp1");
-		//-----------------
-		AssegnamentoManager instance= AssegnamentoManager.getInstance();
-		assertTrue(instance.removeResponsabile(ass));
+		repository.add(oracle);
+		manager.removeResponsabile(oracle);
+		Assegnamento result=repository.findItemByQuery(new AssegnamentoSql(oracle.getLaboratorio(),oracle.getResponsabile()));
+		assertEquals(result.getLaboratorio(),"");
 	}
 	
 	@Test
 	public void testShowResponsabileAndLaboratorio() throws SQLException{
 		System.out.println("removeResponsabile");
-		Assegnamento ass= new Assegnamento();
-		ass.setLaboratorio("Lab1");
-		ass.setResponsabile("resp1");
-		List<Assegnamento> assegnamenti= new ArrayList<Assegnamento>();
-		//-----------------
-		AssegnamentoManager instance= AssegnamentoManager.getInstance();
-		instance.setRespToLab(ass);
-		//assegnamenti=instance.showResponsabileAndLaboratorio();
-		assertTrue(!assegnamenti.isEmpty());
+		
 	}
 
 }
