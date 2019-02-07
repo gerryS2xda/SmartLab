be568010d7d8ea5fcffb63f77ee57b21995249dd
AntonioCerasuolo <AntonioCerasuolo@users.noreply.github.com>
2019-02-07 15:04:13 +0100

    a

--- test/postazioneTest/PostazioneSqlTest.java
+++ test/postazioneTest/PostazioneSqlTest.java
@@ -14,8 +14,6 @@ public class PostazioneSqlTest {
         System.out.println("getInstance");
         PostazioneSql result = PostazioneSql.getInstance(1,"");
         assertNotNull(result);
-        
-        
 	}
 	
 	@Test
